071922f6182bfbaed9c76e9bfca1085fdb76ea6d
OliPicard <oliver@ufomail.co.uk>
2015-01-17 16:44:44 +0000

    StopWatch 5.1 Release

---
+++ StopWatch5.1/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace StopWatch5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Lap = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(107, 226);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start/Stop";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(107, 197);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Reset";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(49, 98);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(192, 48);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "00:00:00";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label1.UseWaitCursor = true;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(74, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Lap 1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(125, 27);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Lap 2";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(176, 27);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Lap 3";
+            // 
+            // Lap
+            // 
+            this.Lap.Location = new System.Drawing.Point(107, 167);
+            this.Lap.Name = "Lap";
+            this.Lap.Size = new System.Drawing.Size(75, 23);
+            this.Lap.TabIndex = 4;
+            this.Lap.Text = "lap";
+            this.Lap.UseVisualStyleBackColor = true;
+            this.Lap.Click += new System.EventHandler(this.Lap_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(50, 146);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 13);
+            this.label5.TabIndex = 5;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.label6.Location = new System.Drawing.Point(1, 239);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(100, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Crafted by OliPicard";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.Lap);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Stop Watch 5.1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button Lap;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ StopWatch5.1/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Globalization;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StopWatch5
+{
+    public partial class Form1 : Form
+    {
+        TimeSpan StartTime = new TimeSpan(0, 0, 0);
+        TimeSpan addTime = new TimeSpan(0, 0, 1);
+        int apple;
+        int pear;
+        int cherry;
+        int lp = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            StartTime = StartTime + addTime;
+            label1.Text = StartTime.ToString();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = !timer1.Enabled;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            StartTime = new TimeSpan(0, 0, 0);
+            label1.Text = StartTime.ToString();
+            label2.Text = ("Lap 1");
+            label3.Text = ("Lap 2");
+            label4.Text = ("Lap 3");
+            label5.Text = ("");
+            lp = 0;
+            Lap.Enabled = true;
+        }
+        public void Lap_Click(object sender, EventArgs e)
+        {
+            lp++;
+            timer1.Enabled = false;
+            switch (lp)
+            {
+                case 1:
+                    label2.Text = StartTime.ToString();
+                    string what = StartTime.ToString();
+                    what = what.Replace(":", "");
+                    apple = Convert.ToInt16(what);
+                    calcFTOne();
+                    StartTime = new TimeSpan(0, 0, 0);
+                    label1.Text = StartTime.ToString();
+                    timer1.Enabled = true;
+                    break;
+                case 2:
+                    label3.Text = StartTime.ToString();
+                    string who = StartTime.ToString();
+                    who = who.Replace(":", "");
+                    pear = Convert.ToInt16(who);
+                    calcFTTwo();
+                    StartTime = new TimeSpan(0, 0, 0);
+                    label1.Text = StartTime.ToString();
+                    timer1.Enabled = true;
+                    break;
+                case 3:
+                    Lap.Enabled = false;
+                    label4.Text = StartTime.ToString();
+                    string why = StartTime.ToString();
+                    why = why.Replace(":", "");
+                    cherry = Convert.ToInt16(why);
+                    calcFTThree();
+                    label1.Text = StartTime.ToString();
+                    timer1.Enabled = true;
+                    break;
+            }
+
+        }
+        public void calcFTOne()
+        {
+
+            if (apple == apple)
+            {
+                label5.Text = ("Fastest Lap 1: " + StartTime);
+            }
+        }
+        public void calcFTTwo()
+        {
+            if (pear < apple)
+            {
+                label5.Text = ("Fastest Lap: " + StartTime);
+            }
+            if (pear == apple)
+            {
+                label5.Text = ("Tie between Lap 1 and Lap 2: " + StartTime);
+            }
+        }
+        public void calcFTThree()
+        {
+            if (cherry < apple && cherry < pear)
+            {
+                label5.Text = ("Fastest Lap 3: " + StartTime);
+            }
+            if (cherry == (apple & pear))
+            {
+                label5.Text = ("Tie between Lap 1, 2 and 3! " + StartTime);
+            }
+
+        }
+
+    }
+}
